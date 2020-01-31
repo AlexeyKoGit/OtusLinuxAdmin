0c8e010b278ae5ae9055f7a47a9c6fa037847c49
AlexeyKoGit <alanatolich@mail.ru>
2020-01-31 22:41:17 +0300

    -

--- 02/homework_ss/raid1_step1.sh
+++ 02/homework_ss/raid1_step1.sh
@@ -8,7 +8,8 @@ NORMAL='\033[0m'
 #start
 echo -e "${WHITE}╔═════════════════════════════╗${NORMAL}\n${WHITE}║        RAID1 Step 1         ║${NORMAL}\n${WHITE}╚═════════════════════════════╝${NORMAL}"
 #sudo sudo setenforce 0
-echo -e "${WHITE}========== Install Mdadm${NORMAL}"
+echo -e "${WHITE}========== lsblk Первый.${NORMAL}"
+lsblk --output NAME,FSTYPE,MAJ:MIN,RM,SIZE,RO,TYPE,UUID,MOUNTPOINT
 #1
 echo -e "${WHITE}========== Detect Mdadm${NORMAL}"
 v_tmp=`sudo yum list installed | grep -c 'mdadm' 2>/dev/null`
@@ -17,11 +18,14 @@ v_tmp=`sudo yum list installed | grep -c 'mdadm' 2>/dev/null`
 if [ $v_tmp = 0 ]; then echo -e "${RED}[ERROR]${NORMAL} Mdadm установить не удалось\nРабота завершена с ошибкой"; exit; else echo -e "Mdadm ${GREEN}установлен${NORMAL}"; fi
 #2
 echo -e "${WHITE}========== Copy partition table sda->sdb${NORMAL}"
-v_tmp=`sudo sfdisk -d /dev/sda | sfdisk /dev/sdb  2>/dev/null`
+sudo sfdisk -d /dev/sda | sudo sfdisk /dev/sdb
+wait
 echo -e "${WHITE}========== Change partition identifier on 0xFD (Linux raid autodetect) /dev/sdb${NORMAL}"
 v_tmp=`(echo t; echo fd; echo w) | sudo fdisk /dev/sdb`
+wait
 echo -e "${WHITE}========== Creating RAID1${NORMAL}"
 yes y | sudo mdadm --create /dev/md0 --level=1 --raid-disk=2 missing /dev/sdb1
+wait
 sleep 10
 echo -e "${WHITE}========== Format type FS xfs${NORMAL}"
 v_tmp=`sudo mkfs.xfs /dev/md0`
@@ -29,24 +33,25 @@ uuid_root_part=`lsblk --output UUID,MOUNTPOINT | grep -P ' /$' | grep -oP '^.{36
 uuid_raid_part=`lsblk --output UUID,TYPE | grep -P 'raid' | grep -oP '^.{36}'`
 cmd_l=`grep -P 'GRUB_CMDLINE_LINUX' /etc/default/grub`
 cmd_ln=`echo "$cmd_l" | sed 's/\(\"\\s*\)$/ rd.auto=1\"/'`
+echo -e "${WHITE}========== /etc/default/grub rd.auto=1${NORMAL}"
 sudo sed -i 's/'"$cmd_l"'/'"$cmd_ln"'/g' /etc/default/grub
 cat /etc/default/grub
 #echo "$root $uuid_root_part"
 #echo "$raid $uuid_raid_part"
 #echo "$cmd_l"
 #echo "$cmd_ln"
-
+echo -e "${WHITE}========== copy root(/)${NORMAL}"
 sudo mount /dev/md0 /mnt/
 sudo cp -dpRxf --preserve=context / /mnt/
 
-lsblk --output NAME,FSTYPE,MAJ:MIN,RM,SIZE,RO,TYPE,UUID,MOUNTPOINT
+#lsblk --output NAME,FSTYPE,MAJ:MIN,RM,SIZE,RO,TYPE,UUID,MOUNTPOINT
 
 sudo mount --bind /proc /mnt/proc
 sudo mount --bind /dev /mnt/dev
 sudo mount --bind /sys /mnt/sys
 sudo mount --bind /run /mnt/run
 #sudo chroot /mnt/
-echo -e "${WHITE}=========== chroot  ===========${NORMAL}"
+echo -e "${WHITE}========== chroot${NORMAL}"
 cat << EOF | sudo chroot /mnt 
 mdadm --detail --scan > /etc/mdadm.conf
 sed -i 's|'"$uuid_root_part"'|'"$uuid_raid_part"'|g' /etc/fstab
@@ -57,4 +62,7 @@ grub2-mkconfig -o /boot/grub2/grub.cfg
 grub2-install /dev/sdb
 grub2-install /dev/sda
 EOF
-echo "==RAID=="
+#echo "==RAID=="
+echo -e "${WHITE}========== lsblk${NORMAL}"
+lsblk --output NAME,FSTYPE,MAJ:MIN,RM,SIZE,RO,TYPE,UUID,MOUNTPOINT
+echo -e "${GREEN}Step 1 is complete. Reboot BOX and run script /vagrant/raid1_step2.sh${NORMAL}"
--- 02/homework_ss/raid1_step2.sh
+++ 02/homework_ss/raid1_step2.sh
@@ -7,9 +7,11 @@ GREEN='\033[1;92;40m'
 NORMAL='\033[0m'
 #start
 echo -e "${WHITE}╔═════════════════════════════╗${NORMAL}\n${WHITE}║        RAID1 Step 2         ║${NORMAL}\n${WHITE}╚═════════════════════════════╝${NORMAL}"
-exit
+echo -e "${WHITE}========== Change partition identifier on 0xFD (Linux raid autodetect) /dev/sda${NORMAL}"
 (echo t; echo fd; echo w) | sudo fdisk /dev/sda
-sudo mdadm —add /dev/md0 /dev/sda
-sudo mdadm /dev/md0 --add /dev/sda1
+echo -e "${WHITE}========== Add disk to RAID1${NORMAL}"
+sudo sudo mdadm /dev/md0 --add /dev/sda1
 sudo watch cat /proc/mdstat
-
+echo -e "${WHITE}========== lsblk Финальный${NORMAL}"
+lsblk --output NAME,FSTYPE,MAJ:MIN,RM,SIZE,RO,TYPE,UUID,MOUNTPOINT
+echo -e "${GREEN}OS move to RAID1 complete${NORMAL}"
