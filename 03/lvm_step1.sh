423a4fa8eda8b8b090e0b499195551c55f286958
AlexeyKoGit <alanatolich@mail.ru>
2020-02-12 03:38:58 +0300

    -

--- 03/lvm_step1.sh
+++ 03/lvm_step1.sh
@@ -33,19 +33,48 @@ sudo vgcreate vg_home /dev/$disk_home
 #for i in $disk_var_t; do
 #sudo vgcreate vg_var /dev/$i
 #done
-sudo vgcreate vg_var /dev/${disk_var_t[0]}
-sudo vgextend vg_var /dev/${disk_var_t[1]}
+sudo vgcreate vg_var /dev/${disk_var_t[0]} /dev/${disk_var_t[1]}
+#sudo vgextend vg_var /dev/${disk_var_t[1]}
 echo -e "${WHITE}════════ LVM Physical Volume and Group${NORMAL}"
 sudo pvscan
 echo -e "${WHITE}════════ LVM Creating Logical Volumes${NORMAL}"
 sudo lvcreate -n lv_tmp_root -l +100%FREE /dev/vg_tmp_root
 sudo lvcreate -n lv_home -l +50%FREE /dev/vg_home
-#
-sudo lvcreate -s -n s_shot_home -l +100%FREE /dev/vg_home/lv_home
-
-#sudo lvcreate -L10G -s -n snaphot_test.local /dev/vmstore/test.local
-
-
-
-
-
+# snap shot
+sudo lvcreate -s -n s_shot_lv_home -l +100%FREE /dev/vg_home/lv_home
+#sudo lvcreate -n lv_var -l +50%FREE /dev/vg_var
+#sudo lvcreate -n lv_var -l 250 /dev/vg_var
+# mirror
+sudo lvcreate -m1 -n mirror_lv_var -l +100%FREE /dev/vg_var
+#lvcreate -L 50G -m1 -n mirrorlv vg0
+echo -e "${WHITE}════════ LVM Result${NORMAL}"
+echo "/root"
+sudo lvdisplay | grep -A15 '/lv_tmp_root' | grep -P '(/lv_tmp_root|LV Size)'
+echo "/home"
+sudo lvdisplay | grep -A15 '/lv_home' | grep -P '(/lv_home|LV Size)'
+echo "/home (snapshot)"
+sudo lvdisplay | grep -A15 '/s_shot_lv_home' | grep -P '(/s_shot_lv_home|LV Size|LV snapshot status.+active)'
+echo "/var (mirror)"
+sudo lvdisplay | grep -A15 '/mirror_lv_var' | grep -P '(/mirror_lv_var)'
+sudo lvdisplay | grep -A15 '/mirror_lv_var' | grep -P '(Mirrored volumes)'
+sudo lvdisplay | grep -A15 '/mirror_lv_var' | grep -P '(LV Size)'
+echo -e "${WHITE}════════ Creating filesystem${NORMAL}"
+v_tmp=`sudo mkfs.xfs /dev/vg_tmp_root/lv_tmp_root 2>/dev/null`
+v_tmp=`sudo mkfs.ext4 /dev/vg_home/lv_home 2>/dev/null`
+v_tmp=`sudo mkfs.ext4 /dev/vg_home/s_shot_lv_home 2>/dev/null`
+v_tmp=`sudo mkfs.ext4 /dev/vg_var/mirror_lv_var 2>/dev/null`
+blkid | grep -P '(xfs|ext4)' | grep '/vg_'
+echo -e "${WHITE}════════ Creating Mount Points${NORMAL}"
+sudo mkdir /mnt/v_tmp_root
+sudo mkdir /mnt/v_home
+sudo mkdir /mnt/v_var
+ls -l /mnt/
+echo -e "${WHITE}════════ Mount LVM Volumes${NORMAL}"
+sudo mount /dev/vg_tmp_root/lv_tmp_root/ /mnt/v_tmp_root
+sudo mount /dev/vg_home/lv_home/ /mnt/v_home
+sudo mount /dev/vg_var/mirror_lv_var/ /mnt/v_var
+mount | grep '/vg_'
+echo -e "${WHITE}════════ Copying Data${NORMAL}"
+sudo cp -dpRxf --preserve=context / /mnt/v_tmp_root/
+sudo cp -dpRf --preserve=context /home /mnt/v_home/
+sudo cp -dpRf --preserve=context /var /mnt/v_var/
