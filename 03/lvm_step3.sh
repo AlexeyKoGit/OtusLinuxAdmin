f766421e080a980e81365c9e69cf0f1b039c43ab
AlexeyKoGit <alanatolich@mail.ru>
2020-02-16 05:54:35 +0300

    -

--- 03/lvm_step3.sh
+++ 03/lvm_step3.sh
@@ -6,6 +6,17 @@ YELLOW='\033[1;93;40m'
 GREEN='\033[1;92;40m'
 NORMAL='\033[0m'
 #start
-label="${WHITE}║        LVM STEP 2           ║${NORMAL}"
+label="${WHITE}║        LVM STEP 3           ║${NORMAL}"
 echo -e "${WHITE}╔═════════════════════════════╗${NORMAL}\n${WHITE}$label${NORMAL}\n${WHITE}╚═════════════════════════════╝${NORMAL}"
-sudo lvs
+for (( i=1; i <= 15; i++ ))
+do
+cat > /home/vagrant/$i.html << EOF
+$RANDOM
+EOF
+#$RANDOM > /home/vagrant/$i.txt
+done
+
+#sudo vgrename -v vg_tmp_root fs_lab
+#sudo lvrename /dev/fs_lab/lv_tmp_root /dev/fs_lab/lv_zfs
+
+#udo lvs
