cf43d616d0b90904bc8ee0f2aaf04a75c79c28a2
AlexeyKoGit <alanatolich@mail.ru>
2020-03-06 11:33:26 +0300

    -

--- 03/homework_s/zfs_step1.sh
+++ 03/homework_s/zfs_step1.sh
@@ -10,34 +10,16 @@ NORMAL='\033[0m'
 label="${WHITE}║        LVM STEP 1           ║${NORMAL}"
 echo -e "${WHITE}╔═════════════════════════════╗${NORMAL}\n${WHITE}$label${NORMAL}\n${WHITE}╚═════════════════════════════╝${NORMAL}"
 lsblk
-sudo yum -y install http://download.zfsonlinux.org/epel/zfs-release.el7_5.noarch.rpm
-sudo yum -y upgrade
+echo -e "${WHITE}════════ Install ZFS${NORMAL}"
+v_tmp=`sudo yum -y install http://download.zfsonlinux.org/epel/zfs-release.el7_5.noarch.rpm`
+echo -e "zfs      ${YELLOW}disabled${NORMAL} repo"
+echo -e "zfs-kmod ${GREEN}enabled${NORMAL}  repo"
+
 sudo python -c "import ConfigParser;
 config = ConfigParser.RawConfigParser();
 config.read('/etc/yum.repos.d/zfs.repo');
 config.set('zfs', 'enabled', 0);
 config.set('zfs-kmod', 'enabled', 1);
 with open('/etc/yum.repos.d/zfs.repo', 'wb') as configfile: config.write(configfile)"
-
-sudo yum -y install zfs
-
-#sudo python -c "import ConfigParser;config = ConfigParser.RawConfigParser();config.read('/etc/yum.repos.d/zfs.repo');print(config.sections())" 
-#sudo yum -y install epel-release
-#sudo yum -y localinstall http://download.zfsonlinux.org/epel/zfs-release.el7_7.noarch.rpm
-#sudo yum -y install --skip-broken zfs
-
-
-#sudo yum -y install zfs
-
-
-	
-#sudo python - << EOF
-#import ConfigParser
-#config = ConfigParser.RawConfigParser()
-#config.read('/etc/yum.repos.d/zfs.repo')
-#config.set('zfs', 'enabled', 0)
-#config.set('zfs-kmod', 'enabled', 1)
-#with open('example.cfg', 'wb') as configfile:
-#	config.write('/etc/yum.repos.d/zfs.repo')
-#EOF
-#
+echo " Please wait for the installation ZFS-KMOD"	
+sudo yum -y install zfs | grep -Poz '((?s)([=]{80}).+[=]{80}.(?-s)Install.+$)|(^Running transaction$)|(Installing.+$)'
